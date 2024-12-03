16ab59725c6ce0b27062fafca48361a0410498fa
PROTOTYPE9111 <prototype9111@list.ru>
2024-12-04 02:56:58 +0300

    modified:   README.md 	new file:   project_1.1.cpp 	new file:   project_1.2.1.cpp 	new file:   project_1.2.cpp 	new file:   project_1.3.cpp 	new file:   project_1.cpp

---
+++ project_1.1.cpp
@@ -0,0 +1,14 @@
+// shildtbooklearn (p.45)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int length; //Объявление переменной
+    length = 7; // Переменной присваиваем число 7
+
+    cout << "The length is ";
+    cout << length; //Вывод числа 7
+
+    return 0;
+}
