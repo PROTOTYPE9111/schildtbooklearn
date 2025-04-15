056c9b93d5dcc2663724779194b549ea46dda995
PROTOTYPE9111 <prototype9111@list.ru>
2025-04-15 23:53:32 +0300

    новые файлы

---
+++ module_4/project_4.10.cpp
@@ -0,0 +1,18 @@
+//schildtbooklearn (p.175)
+// присваивание 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int *p, num;
+
+    p = &num;
+    
+    *p = 100;
+    cout << num << endl;
+    (*p)++; 
+    cout << num << endl;
+    (*p)--;
+    cout << num << endl;
+}
