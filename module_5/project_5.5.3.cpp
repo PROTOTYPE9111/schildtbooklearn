0f1ed591918b811b7a074f0337ef7eaf07024412
PROTOTYPE9111 <prototype9111@list.ru>
2025-04-26 22:17:05 +0300

    Новые файлы

---
+++ module_5/project_5.5.3.cpp
@@ -0,0 +1,21 @@
+//schildtbooklearn (p.216)
+// передача массива в функцию 3 способ
+
+#include <iostream>
+using namespace std;
+
+void display(int num[10]);
+
+int main(){
+    int t[10], i; 
+    for(i = 0; i < 10; ++i) t[i] = i;
+
+    display(t); // передаем функции массив t
+}
+
+// функция выводит все элементы массива
+
+void display(int *num){ //параметр объявлен как безразмерный массив
+    int i;
+    for(i = 0; i < 10; i++) cout << num[i] << endl;
+}
