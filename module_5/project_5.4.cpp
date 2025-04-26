19b41187e40c5932b2928d758c4c4614f6d45baa
PROTOTYPE9111 <prototype9111@list.ru>
2025-04-26 11:19:19 +0300

    Добавил файл

---
+++ module_5/project_5.4.cpp
@@ -0,0 +1,23 @@
+//schildtbooklearn (p.213)
+// передача функции указателя
+
+#include <iostream>
+
+using namespace std;
+
+void f(int *j); // Функция объявляет параметр указатель
+int main(){
+    int i;
+    int *p;
+
+    p = &i; // указатель p ссылается на переменную i
+    
+    f(p); // передаем указатель
+
+    cout << i; // теперь содержит 100
+}
+
+void f(int *j){
+    *j = 100;
+}
+
