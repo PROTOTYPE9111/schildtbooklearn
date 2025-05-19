f12a4c6700108d4ba5e2fcc72d63e66536b94ae7
grn13 <grn1305>
2025-05-19 21:22:19 +0300

    Добавил новые файлы

---
+++ grn_13_learning_folder/function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    int result;
+    result = abs(-10); //вызывается функция абс(), а возвращаемое ею значение присваивается переменной result
+    cout << result;
+    return 0;
+}
