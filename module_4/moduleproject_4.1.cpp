382ef4e233e219d9a9f0e0e62d512bc759f465fc
Магомед <prototype9111@list.ru>
2026-08-09 16:07:52 -0700

    new

---
+++ module_4/moduleproject_4.1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    int nums[10];
+    int a,b,t;
+    int size;
+
+    size = 10; // количество сортируемых элементов
+
+    // Помещаем массив в случайные числа
+    for(t = 0; t < size; t++) nums[t] = rand();
+
+    // Отображаем исходный массив 
+    cout << "Orig is: " << endl;
+    for(t = 0; t < size; t++) cout << nums[t] <<' ';
+    cout << endl;
+
+    // Реализация алгоритма пузырьковой сборки
+    for(a=1; a < size; a++)
+        for(b = size-1; b >= a; b--){
+            if(nums[b-1] > nums[b]){
+                t = nums[b-1];
+                nums[b-1] = nums[b];
+                nums[b] = t;
+            }
+        }
+        //Отображем отсированный массив
+        cout << endl;
+        for(t = 0; t < size; t++) cout << nums[t];
+        return 0;
+}
---
+++ module_4/project_4.4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char str[80];
+    cout << "Enter a string: ";
+    cin >> str; //считывание строки с клавиатуры
+    cout << "Here is yours: ";
+    cout << str;
+
+    return 0;
+}
